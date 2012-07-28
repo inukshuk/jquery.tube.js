3ca6ab27b0441d5667a7c43fcc9633116162725c
Sylvester Keil <sylvester@keil.or.at>
2012-07-28 16:56:26 +0200

    added video loader as plugin function

--- jquery.tube.js
+++ jquery.tube.js
@@ -1082,18 +1082,18 @@
   // The jQuery fn plugin for playlists/queries
   $.fn.tube = function (args) {
     var element, options;
-    
+  
     if (this.length) {
-      
+  
       if (typeof args === 'string') {
         options = { query: args };
       }
       else {
         options = args;
       }
-      
+  
       element = this.first();
-      
+  
       element.data('tube', new Tube(options).load(function (success) {
         var tube = this, playlist;
   
@@ -1112,69 +1112,74 @@
             if ($.isFunction(tube.options.load)) {
               tube.options.load.apply(tube, ['load', playlist, element]);
             }
-          
-            element.append(playlist); 
-    
+  
+            element.append(playlist);
+  
           }
-    
+  
           if ($.isFunction(tube.options.complete)) {
             tube.options.complete.apply(tube, ['complete', playlist, element]);
           }
-    
+  
         }
         else {
           if ($.isFunction(tube.options.complete)) {
             tube.options.complete.apply(tube, ['error', playlist, element]);
           }
         }
-        
+  
       }));
-      
+  
     }
-    
+  
     return this;
   };
   
   // The jQuery fn plugin for single player instances
   $.fn.player = function (args) {
     var element, options, player;
-    
+  
     if (this.length) {
-      
+  
       if (typeof args === 'string') {
         options = $.extend({}, $.player.defaults, { video: args });
       }
       else {
         options = $.extend({}, $.player.defaults, args);
       }
-      
+  
       element = this.first();
-      
+  
       if (element.data('player')) {
         element.data('player').play(options.video);
       }
       else {
-        
+  
         options.id = element.attr('id');
-        
+  
         // If the element does not have an id, generate one.
         // NB: we use a simple counter here and no collision detection!
         if (!options.id) {
           element.attr('id', 'jquery-tube-player-' + ($.player.counter++));
           options.id = element.attr('id');
         }
-                    
+  
         if (options.video) {
           Player.create(options).load(options.video);
-        }      
+        }
       }
     }
-    
+  
     return this;
-    
+  
   };
   
   $.tube = {};
+  
+  // A jQuery plugin that loads a single video by id
+  $.tube.load_video = function (id, callback) {
+    return (new Video()).load(id, callback);
+  };
   
   $.tube.constants = Tube.constants;
   $.tube.defaults = Tube.defaults;
--- src/plugin.js
+++ src/plugin.js
@@ -2,18 +2,18 @@
 // The jQuery fn plugin for playlists/queries
 $.fn.tube = function (args) {
   var element, options;
-  
+
   if (this.length) {
-    
+
     if (typeof args === 'string') {
       options = { query: args };
     }
     else {
       options = args;
     }
-    
+
     element = this.first();
-    
+
     element.data('tube', new Tube(options).load(function (success) {
       var tube = this, playlist;
 
@@ -32,69 +32,74 @@ $.fn.tube = function (args) {
           if ($.isFunction(tube.options.load)) {
             tube.options.load.apply(tube, ['load', playlist, element]);
           }
-        
-          element.append(playlist); 
-  
+
+          element.append(playlist);
+
         }
-  
+
         if ($.isFunction(tube.options.complete)) {
           tube.options.complete.apply(tube, ['complete', playlist, element]);
         }
-  
+
       }
       else {
         if ($.isFunction(tube.options.complete)) {
           tube.options.complete.apply(tube, ['error', playlist, element]);
         }
       }
-      
+
     }));
-    
+
   }
-  
+
   return this;
 };
 
 // The jQuery fn plugin for single player instances
 $.fn.player = function (args) {
   var element, options, player;
-  
+
   if (this.length) {
-    
+
     if (typeof args === 'string') {
       options = $.extend({}, $.player.defaults, { video: args });
     }
     else {
       options = $.extend({}, $.player.defaults, args);
     }
-    
+
     element = this.first();
-    
+
     if (element.data('player')) {
       element.data('player').play(options.video);
     }
     else {
-      
+
       options.id = element.attr('id');
-      
+
       // If the element does not have an id, generate one.
       // NB: we use a simple counter here and no collision detection!
       if (!options.id) {
         element.attr('id', 'jquery-tube-player-' + ($.player.counter++));
         options.id = element.attr('id');
       }
-                  
+
       if (options.video) {
         Player.create(options).load(options.video);
-      }      
+      }
     }
   }
-  
+
   return this;
-  
+
 };
 
 $.tube = {};
+
+// A jQuery plugin that loads a single video by id
+$.tube.load_video = function (id, callback) {
+  return (new Video()).load(id, callback);
+};
 
 $.tube.constants = Tube.constants;
 $.tube.defaults = Tube.defaults;
